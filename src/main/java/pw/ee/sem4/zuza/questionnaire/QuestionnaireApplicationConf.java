5b1c65a2948e5e0855fa834169e77c1738555e2d
zvzia <gaik.zuza@gmail.com>
2022-06-11 15:48:27 +0200

    polskie znaki nie dzialaly w jednym miejscu

--- src/main/java/pw/ee/sem4/zuza/questionnaire/QuestionnaireApplicationConf.java
+++ src/main/java/pw/ee/sem4/zuza/questionnaire/QuestionnaireApplicationConf.java
@@ -40,6 +40,7 @@ public class QuestionnaireApplicationConf {
         templateResolver.setPrefix("/WEB-INF/templates/");
         templateResolver.setSuffix(".html");
         templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding("UTF-8");
 
         return templateResolver;
     }
